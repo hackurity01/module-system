ecc6aa7aef0452d08a976a317c7dd87cada62825
hackurity01 <hackurity01@gmail.com>
2024-03-20 13:59:03 +0900

    eslint test .

---
+++ 6_cra/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+// var test = 123;
+// d;
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">test</header>
+    </div>
+  );
+}
+
+export default App;
